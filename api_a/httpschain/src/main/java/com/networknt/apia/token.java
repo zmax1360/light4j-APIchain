dd0476c796beb302162dc5f71b91658eb6271534
ZEUS\Ali <zafari.a@gmail.com>
2018-04-01 13:19:15 -0400

    changing the api-c&api_d client and secret

--- api_a/httpschain/src/main/java/com/networknt/apia/token.java
+++ api_a/httpschain/src/main/java/com/networknt/apia/token.java
@@ -11,7 +11,7 @@ public class token {
         longLivedAPICJwt();
     }
     public static void longLivedAPICJwt() throws Exception {
-        JwtClaims claims = getTestClaims("Steve", "EMPLOYEE", "2b5f158d-1e86-4e44-bf1d-ca6612af624d", Arrays.asList("api_a.r", "api_b.r", "api_c.r", "api_d.r","api_a.w", "api_b.w", "api_c.w", "api_d.w", "server.info.r"));
+        JwtClaims claims = getTestClaims("Steve", "EMPLOYEE", "40ada0a7-d457-4d16-b6e0-80adae4bc06f", Arrays.asList("api_c.r", "api_c.w"));
         claims.setExpirationTimeMinutesInTheFuture(5256000);
         String jwt = JwtHelper.getJwt(claims);
         System.out.println("***LongLived APIC JWT***: " + jwt);
